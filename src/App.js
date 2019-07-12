3c443986db046816046bc6febe59f086b176a1b6
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-12 07:52:58 -0700

    added custom fonts and assets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import './App.css';
 
+const SearchBox=(props)=>{
+  return(
+    <div className="searchDiv">
+      <label>Search for Robots:</label>
+      <div><input onChange={props.onChange}/></div>
+    </div>
+  )
+}
+
 const Card=(props)=>{
+  
   return(
     <div className="card">
-      <h1>Character</h1>
-      <img src={`https://robohash.org/${props.image}?200x200`} alt={`${props.name}`}/>
-      <h2>Name: {props.name}</h2>
-      <h3>height: {props.height}</h3>
+      <h1 className="h1Card">{props.name}</h1>
+      <img 
+      className={props.background}
+      src={`https://robohash.org/${props.image}?200x200`} 
+      alt={`${props.name}`}
+
+      />
+      <h3>Height: {props.height}</h3>
       <h3>Mass: {props.mass}</h3>
     </div>
   )
 }
 
-const CardList=({jedi})=>{
+
+
+const CardList=({jedi,background})=>{
   const cardComponent= jedi.map((el,i)=>{
      return( 
      <Card 
-        image={i}
-        key={el}
+        image={el.url.replace(/[^/0-9]/g,'')}
+        background={`${background[Math.floor(Math.random()*background.length)]}`}
+        key={i}
         name={el.name}
         height={el.height}
         mass={el.mass}
@@ -37,8 +54,14 @@ class App extends React.Component {
 constructor(){
     super();
      this.state={
-      jedi:[]
+      jedi:[],
+      searchName:'',
+      background:["image0","image1","image2"]
     }
+}
+
+onSearchChange=(event)=>{
+  this.setState({searchName:event.target.value})
 }
 
 
@@ -50,12 +73,24 @@ componentDidMount(){
 
 
   render(){ 
-     return(
+      let jedi=this.state.jedi;
+      let searchName=this.state.searchName;
+      let background=this.state.background;
       
-      <div className="App">
-        <header className="header">Robots from Star Wars</header>
-        <CardList jedi={this.state.jedi}/>
-      </div>
+
+      let filteredJedi=jedi.filter(char=>{
+          return char.name.toLowerCase().includes(searchName.toLowerCase())
+      });
+
+
+     return(
+         <div className="App">
+          <h1 className="header">Robots from Star Wars
+          </h1>
+          <SearchBox onChange={this.onSearchChange}/>
+          <CardList jedi={filteredJedi} background={background}/>
+        </div>
+   
     )
   }
 }
