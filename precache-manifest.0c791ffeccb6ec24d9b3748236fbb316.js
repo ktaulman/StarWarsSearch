c73096c604b76ecccc4056be858563079dd48338
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-12 12:11:12 -0700

    Updates

---
+++ precache-manifest.0c791ffeccb6ec24d9b3748236fbb316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d2a5dceb3ab88bf9e1e0a48e23f52c",
+    "url": "/StarWarsSearch/index.html"
+  },
+  {
+    "revision": "327fb47f3fe95315e483",
+    "url": "/StarWarsSearch/static/css/main.cb3b0516.chunk.css"
+  },
+  {
+    "revision": "74dea638f0d10f414654",
+    "url": "/StarWarsSearch/static/js/2.4c0f75d4.chunk.js"
+  },
+  {
+    "revision": "327fb47f3fe95315e483",
+    "url": "/StarWarsSearch/static/js/main.e62cb417.chunk.js"
+  },
+  {
+    "revision": "4ef4c1b9097e7c8f90d3",
+    "url": "/StarWarsSearch/static/js/runtime~main.2be164a3.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/StarWarsSearch/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
